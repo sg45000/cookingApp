dc14af3f50b5637c29f8b26ced045c7020803253
kyo <hamakyo711@gmail.com>
2018-11-28 20:48:45 +0900

    Add box user entity

---
+++ src/model/Box.java
@@ -0,0 +1,87 @@
+package model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="box")
+public class Box {
+
+    @Id
+    @Column(name="id" ,nullable=false)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name="user_id" ,nullable=false)
+    private Integer user_id;
+
+    @Column(name="material_id" ,nullable=false)
+    private Integer material_id;
+
+    @Column(name="quantity" ,nullable=false)
+    private Double quantity;
+
+    @Column(name="created_at" ,nullable=false)
+    private Timestamp created_at;
+
+    @Column(name="updated_at" ,nullable=false)
+    private Timestamp updated_at;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public Integer getMaterial_id() {
+        return material_id;
+    }
+
+    public void setMaterial_id(Integer material_id) {
+        this.material_id = material_id;
+    }
+
+    public Double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Double quantity) {
+        this.quantity = quantity;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+
+
+
+
+}
---
+++ src/model/User.java
@@ -0,0 +1,85 @@
+package model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+
+    @Id
+    @Column(name="user_id" ,nullable=false)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer user_id;
+
+    @Column(name="name" ,nullable=false)
+    private String name;
+
+    @Column(name="email" ,nullable=false)
+    private String email;
+
+    @Column(name="password" ,nullable=false)
+    private String password;
+
+    @Column(name="created_at" ,nullable=false)
+    private Timestamp created_at;
+
+    @Column(name="updated_at" ,nullable=false)
+    private Timestamp updated_at;
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+
+
+}
